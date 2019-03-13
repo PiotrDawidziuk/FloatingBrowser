7145da841d91538804d161cefc8f3ef951b50000
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-03-13 18:14:54 +0100

    menu added, makes window float

--- app/src/main/java/pl/piotrdawidziuk/floatingbrowser/MainActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/floatingbrowser/MainActivity.java
@@ -3,14 +3,47 @@ package pl.piotrdawidziuk.floatingbrowser;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void pictureInPicture(View view){
         enterPictureInPictureMode();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(this, "Help selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.floatWindow:
+                Toast.makeText(this, "Float window selected", Toast.LENGTH_SHORT).show();
+                enterPictureInPictureMode();
+                return true;
+
+            default:
+                return false;
+
+        }
     }
 
     @Override
